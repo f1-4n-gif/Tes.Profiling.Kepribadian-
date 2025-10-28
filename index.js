135bf5124fa79141fcdcbd22195245e50f341aa5
F1.4n.gif <alfian27@guru.smp.belajar.id>
2025-10-28 16:44:55 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+const LOGO_URL = "uploaded:Gemini_Generated_Image_rlsntyrlsntyrlsn.jpg-5a1be042-a2dc-4895-9cc7-7cc99ff70d8b";
+
+Aplikasi sekarang menggunakan gambar kepribadian yang Anda unggah sebagai logo. Silakan coba fitur Pratinjau (Preview) untuk melihat perubahannya!
